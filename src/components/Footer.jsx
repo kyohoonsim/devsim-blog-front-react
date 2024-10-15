812b8b7cb583e7f15e1d882e0d90ba83ed90972c
kyohoonsim <kyohoonsim@gmail.com>
2024-10-15 22:06:30 +0900

    ENH: 카피라이트 명 변경

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import "./Footer.css";
 
 const Footer = () => {
-  return <div className="Footer">© devsim</div>;
+  return <div className="Footer">© simVault</div>;
 };
 
 export default Footer;
