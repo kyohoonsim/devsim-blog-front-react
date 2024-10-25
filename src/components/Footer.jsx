fe3034eee83caf57143617964f1c9d6c9e3adb4e
kyohoonsim <kyohoonsim@gmail.com>
2024-10-25 17:18:55 +0900

    ENH: Footer에 블로그 개설 동기 및 기술 스택 설명 추가

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,16 @@ import { useNavigate } from "react-router-dom";
 import "./Footer.css";
 
 const Footer = () => {
-  return <div className="Footer">© simVault</div>;
+  return (
+    <div className="Footer">
+      <div>© simVault</div>
+      <div className="Footer_bottom">
+        네이버, 티스토리 등의 블로그를 오랜 기간 해오면서 항상 저만의 블로그가
+        만들고 싶었습니다. 이 블로그는 react + spring boot + mysql + aws 등의
+        기술로 구현되었습니다.
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
